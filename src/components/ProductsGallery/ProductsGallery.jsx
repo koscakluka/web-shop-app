ed8833c846a83650d6aa6c38e634db2eec13acb8
Luka Košćak <koscak.luka@gmail.com>
2022-08-24 13:24:18 +0300

    Add external checkbox control to ProductsGallery

--- src/components/ProductsGallery/ProductsGallery.jsx
+++ src/components/ProductsGallery/ProductsGallery.jsx
@@ -8,31 +8,41 @@ import {
   ProductCheckbox,
 } from "./ProductsGallery.style";
 
-const ProductsGallery = ({ products }) => {
+const ProductsGallery = ({ products, selectHandler }) => {
   return (
     <ProductsGrid container spacing={8}>
       {products.map((product) => (
         // <ProductsGrid key={product.id}> {/* For proper flex flow */}
         <ProductsGrid item key={product.id} xs={12} sm={6} lg={4}>
-          <Product product={product} />
+          <Product
+            product={product}
+            selectHandler={selectHandler}
+            selected={product.selected}
+          />
         </ProductsGrid>
       ))}
     </ProductsGrid>
   );
 };
 
-const Product = ({ product }) => {
+const Product = ({ product, selectHandler, selected }) => {
+  //TODO: double check sync checking with global cart
+  //potential bug e.g. updating cart in some other place
   const [checked, setChecked] = React.useState(false);
 
   const handleCardClick = () => {
-    setChecked((checked) => !checked);
+    setChecked((checked) => !selected);
+    selectHandler(product.id);
   };
 
   return (
-    <ProductCard onClick={() => handleCardClick()}>
+    <ProductCard onClick={handleCardClick}>
       <ProductImage src={product.images[0]} />
       <ProductTitle>{`${product.title}: $${product.price}`}</ProductTitle>
-      <ProductCheckbox checked={checked} />
+      <ProductCheckbox
+        onClick={handleCardClick}
+        checked={selected || checked}
+      />
     </ProductCard>
   );
 };
